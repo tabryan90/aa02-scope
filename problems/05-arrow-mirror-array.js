92e82f71ebf298faf1cba9f8ea882d105c3d093f
Alex Bryan <tabryan1990@gmail.com>
2024-02-15 23:21:25 -0500

    problem 05 solved. toReveresed() sucks

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -10,8 +10,22 @@ arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
-// Your code here 
+// Your code here
 
+let arrowMirrorArray = (array) => {
+  return [...array, ...array.reverse()];
+
+  // let initArr = array;
+  // let results = [];
+
+  // results = initArr.toReversed();
+
+  // return console.log(results);
+
+}
+
+arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
+arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
