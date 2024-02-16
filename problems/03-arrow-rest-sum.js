2b8f70f7d8d50681940909e02d2d33d2c2c54d27
Alex Bryan <tabryan1990@gmail.com>
2024-02-15 22:50:47 -0500

    problem 03 solved

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -12,6 +12,19 @@ arrowRestSum(0); // => 0
 ***********************************************************************/
 
 // Your code here test
+const arrowRestSum = (...params) => {
+  let arrParams = params;
+  let result = 0;
+  for ( let i = 0; i < arrParams.length; i++ ) {
+     result += arrParams[i];
+  }
+
+  return result;
+}
+
+arrowRestSum(3,5,6); // => 14
+arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
+arrowRestSum(0); // => 0
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
