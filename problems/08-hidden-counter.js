ed9cb6ddae4a8c4b93749a3468696504f0045de7
Alex Bryan <tabryan1990@gmail.com>
2024-02-15 23:53:36 -0500

    problem 08 solved

--- problems/08-hidden-counter.js
+++ problems/08-hidden-counter.js
@@ -15,7 +15,23 @@ hidden2(); // returns 1
 
 ***********************************************************************/
 
-// Your code here 
+// Your code here
+
+function hiddenCounter() {
+  let count = 0;
+
+  return function() {
+    count++;
+    return count;
+  };
+}
+
+let hidden1 = hiddenCounter(); //returns a function
+hidden1(); // returns 1
+hidden1(); // returns 2
+
+let hidden2 = hiddenCounter(); // returns a function
+hidden2(); // returns 1
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
